e2711b03a876e9654220bf96953e3402a8f57221
yoyo930021 <yoyo930021@gmail.com>
2026-04-26 11:11:36 +0800

    chore: configure ESLint flat config and Vitest

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  formatters: true,
+  ignores: ['dist', 'node_modules', '**/*.generated.*', 'landing/src/data/talks.generated.json'],
+})
